14b02daa30e383163438fb8624bc21e544f341f9
emillybatista10 <103061139+emillybatista10@users.noreply.github.com>
2022-09-15 15:44:14 -0300

    Update main.jl

--- terceira aula/main.jl
+++ terceira aula/main.jl
@@ -3,7 +3,6 @@ f1(x)= sqrt(x)-cos(x)
 f2(x)= 5*sin(x^2)-MathConstants.e^(x/10)
 
 error = 10^-4
-
 function bisection(a,b,f)
   x=(a+b)/2
   while abs(f(x)) > error
@@ -16,12 +15,9 @@ function bisection(a,b,f)
   end
   return x
 end
-
 r = bisection(0,1,f1)
 println(r)
-
 r = bisection(0,1,f2)
 println(r)
-
 r = bisection(0,1,f3)
 println(r)
